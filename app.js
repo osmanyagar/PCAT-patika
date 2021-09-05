7b72d56abb3410e1e2fd2d2a588c9e2e9d93aa3e
osmanyagar <osmannuriyagar@gmail.com>
2021-09-06 02:16:11 +0300

    public file created

--- app.js
+++ app.js
@@ -1,15 +1,12 @@
 const express = require('express');
+const path = require('path');
 
 const app = express();
+app.use(express.static('public'));
 
-app.get('/', (req, res) => {
-
-  const photo = {
-    id: 1,
-    name: "Photo Name",
-    description: "Photo description"
-  }
-  res.send(photo)
+app.get('/', (req, res) => {  
+  res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  
 })
 
 const port = 3000;
