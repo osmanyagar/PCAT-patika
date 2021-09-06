30064c394fb03ea87a9bb1ba0afbe04f66e4d9a7
osmanyagar <osmannuriyagar@gmail.com>
2021-09-06 13:35:29 +0300

    html doc edided

--- app.js
+++ app.js
@@ -1,13 +1,25 @@
 const express = require('express');
 const path = require('path');
-
+const ejs = require('ejs');
 const app = express();
+
+//template Engine
+app.set('view engine', 'ejs');
+
+//Middlewares
 app.use(express.static('public'));
 
-app.get('/', (req, res) => {  
-  res.sendFile(path.resolve(__dirname,'temp/index.html'));
-  
-})
+app.get('/', (req, res) => {
+  // res.sendFile(path.resolve(__dirname,'temp/index.html'));
+  res.render('index');
+});
+
+app.get('/about',(req,res) =>{
+  res.render('about');
+});
+app.get('/add',(req,res) =>{
+  res.render('add');
+});
 
 const port = 3000;
 app.listen(port, () => {
